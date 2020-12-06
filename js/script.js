a82db8193e6df278dba1a61a3d1af0979a0cd067
Andrew Sipes <andrew.n.sipes@gmail.com>
2020-12-06 15:47:14 -0500

    Removed superfluous JavaScript.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-/* panels */
-
-const init = function () {
-  let items = document.querySelectorAll("section");
-  for (let i = 0; i < items.length; i++) {
-    items[i].style.background = randomColor({ luminosity: "light" });
-  }
-  cssScrollSnapPolyfill();
-};
-init();
-
 /* Canvas */
 
 var mousePressed = false;
