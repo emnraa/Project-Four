65ceeb3145fa2bf7d39ac8084147eab22817be80
Andrew Sipes <andrew.n.sipes@gmail.com>
2020-12-06 18:54:08 -0500

    Refactor to vanilla JavaScript. Remove jQuery.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,22 @@
-/* Canvas */
+/* Get Elements */
+var canvas = document.querySelector("#myCanvas");
+var clearCanvasButton = document.querySelector("#clear-canvas-button");
+var selColor = document.querySelector("#selColor");
+var selWidth = document.querySelector("#selWidth");
 
+/* Setup Variables */
+var ctx = canvas.getContext("2d");
+var strokeColor = selColor.value;
+var strokeWidth = selWidth.value;
 var mousePressed = false;
 var lastX, lastY;
-var ctx;
 
-ctx = document.getElementById("myCanvas").getContext("2d");
-
-$("#myCanvas").mousedown(function (e) {
-  mousePressed = true;
-  Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-});
-
-$("#myCanvas").mousemove(function (e) {
-  if (mousePressed) {
-    Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-  }
-});
-
-$("#myCanvas").mouseup(function (e) {
-  mousePressed = false;
-});
-$("#myCanvas").mouseleave(function (e) {
-  mousePressed = false;
-});
-
-function Draw(x, y, isDown) {
+/* Define Behavior */
+function draw(x, y, isDown) {
   if (isDown) {
     ctx.beginPath();
-    ctx.strokeStyle = $("#selColor").val();
-    ctx.lineWidth = $("#selWidth").val();
+    ctx.strokeStyle = strokeColor;
+    ctx.lineWidth = strokeWidth;
     ctx.lineJoin = "round";
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(x, y);
@@ -39,13 +27,61 @@ function Draw(x, y, isDown) {
   lastY = y;
 }
 
-var clearCanvasButton = document.querySelector("#clear-canvas-button");
-
 function clearArea() {
   // Use the identity matrix while clearing the canvas
   ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
+
+function getOffset(element) {
+  if (!element.getClientRects().length) {
+    return { top: 0, left: 0 };
+  }
+
+  let rect = element.getBoundingClientRect();
+  let win = element.ownerDocument.defaultView;
+  return {
+    top: rect.top + win.pageYOffset,
+    left: rect.left + win.pageXOffset,
+  };
+}
+
+/* Setup Event Handlers */
+function handleMouseDown(e) {
+  mousePressed = true;
+  draw(e.pageX - getOffset(this).left, e.pageY - getOffset(this).top, false);
+}
+
+function handleMouseMove(e) {
+  if (mousePressed) {
+    draw(e.pageX - getOffset(this).left, e.pageY - getOffset(this).top, true);
+  }
+}
+
+function handleMouseUp(e) {
+  mousePressed = false;
+}
+
+function handleMouseLeave(e) {
+  mousePressed = false;
+}
+
+function handleWidthUpdate(e) {
+  strokeWidth = selWidth.value;
+}
+
+function handleColorUpdate(e) {
+  strokeColor = selColor.value;
+}
+
+/* Wire it up */
+canvas.addEventListener("mousedown", handleMouseDown);
+canvas.addEventListener("mousemove", handleMouseMove);
+canvas.addEventListener("mouseup", handleMouseUp);
+canvas.addEventListener("mouseleave", handleMouseLeave);
+
+selWidth.addEventListener("change", handleWidthUpdate);
+selColor.addEventListener("change", handleColorUpdate);
 
 clearCanvasButton.addEventListener("click", clearArea);
 
